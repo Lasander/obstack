f50296283437aec42f9a2a761089a9e8997f2958
Lasse Lopperi <lasse.lopperi@iki.fi>
2018-10-08 01:09:14 +0300

    Replace tabs with spaces (4)
    * also remove trailing whitespace

--- obstack.cpp
+++ obstack.cpp
@@ -14,40 +14,40 @@ void array_of_primitives_dtor(void*) {
 }
 
 static size_t seed_from_heap_memory() {
-	size_t const len = 64*1024 / sizeof(size_t);
-	size_t * const mem = new size_t[len];
-	size_t seed = reinterpret_cast<size_t>(mem);
-	
-	for(size_t i=0; i<len; i++) {
-		seed ^= mem[i];
-	}
-	
-	delete[] mem;
-	return seed;
+    size_t const len = 64*1024 / sizeof(size_t);
+    size_t * const mem = new size_t[len];
+    size_t seed = reinterpret_cast<size_t>(mem);
+
+    for(size_t i=0; i<len; i++) {
+        seed ^= mem[i];
+    }
+
+    delete[] mem;
+    return seed;
 }
 
 static size_t make_strong_seed() {
-	size_t seed = 0;
-	seed ^= seed_from_heap_memory();
-	//TODO add more seed sources	
-	return seed;
+    size_t seed = 0;
+    seed ^= seed_from_heap_memory();
+    //TODO add more seed sources
+    return seed;
 }
 
 static size_t init_cookie() {
-	std::mt19937 gen;
-	static size_t seed = 0;
-	if(!seed) {
-		seed = make_strong_seed();
-	} else {
-		seed++;
-	}
+    std::mt19937 gen;
+    static size_t seed = 0;
+    if(!seed) {
+        seed = make_strong_seed();
+    } else {
+        seed++;
+    }
 
-	gen.seed(seed);
-	std::uniform_int_distribution<size_t> dist(0, std::numeric_limits<size_t>::max());
+    gen.seed(seed);
+    std::uniform_int_distribution<size_t> dist(0, std::numeric_limits<size_t>::max());
 
-	size_t const cookie = dist(gen);
-	
-	return cookie;
+    size_t const cookie = dist(gen);
+
+    return cookie;
 }
 
 static int invalid_addr_reference;
